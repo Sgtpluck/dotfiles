9563b77909a23132a840843ee10270db79c3a549
davida marion <davida@boundless.com>
2019-06-18 11:22:07 -0700

    setup and symlinks

---
+++ shared/symlinks.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+_symlinks_current_dir="${BASH_SOURCE%/*}"
+
+function dotfiles_location() {
+  echo $(cd $_symlinks_current_dir/.. && pwd)
+}
+
+function symlink_dotfile() {
+  local file=$1
+  local destination=$2
+  local full_file_path="$(dotfiles_location)/$file"
+
+  if [ ! -e $destination ]; then
+    echo "Symlinking $full_file_path -> $destination"
+    ln -s $full_file_path $destination
+  fi
+}
